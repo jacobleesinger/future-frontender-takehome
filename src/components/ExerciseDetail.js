1c37328073f6e97aebf2226f5fd5ea720a19a9f1
jacobleesinger <jacobleesinger@gmail.com>
2019-07-18 14:06:52 -0700

    adds video and styling for exercise detail

--- src/components/ExerciseDetail.js
+++ src/components/ExerciseDetail.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
+import './ExerciseDetail.css';
+
 const ExerciseDetail = ({ exercise }) => {
-  console.log(exercise);
   if (!exercise) {
     return (
       <div className='exercise-detail'>
@@ -9,7 +10,31 @@ const ExerciseDetail = ({ exercise }) => {
       </div>
     );
   }
-  return <div className='exercise-detail'>{exercise.name}</div>;
+
+  const renderVideo = () => {
+    const { video } = exercise;
+    if (!video) {
+      return (
+        <div className='video not-available'>
+          Video not available for this exercise
+        </div>
+      );
+    }
+
+    return (
+      <video src={video.url} width='100%' controls>
+        Your browser does not support the video tag
+      </video>
+    );
+  };
+
+  return (
+    <div className='exercise-detail'>
+      <h2>{exercise.name}</h2>
+      <div className='video'>{renderVideo()}</div>
+      <div className='description'>{exercise.description}</div>
+    </div>
+  );
 };
 
 export default ExerciseDetail;
